06f6345ffa7f889f22e18078d9669481c2d0a078
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-04-28 17:26:55 +0300

    Pass in multiple props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        comment="Nice post"
+        imgSrc={faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        comment="Shit"
+        imgSrc={faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        comment="Mehh.."
+        imgSrc={faker.image.image()}
+      />
     </div>
   );
 };
